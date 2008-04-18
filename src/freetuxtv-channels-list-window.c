3f7571e9d47752a8d0dabf0e90c38352d953f10b
julien39 <julien39@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2008-04-18 05:30:35 +0000

    Essai concernant la fenetre de gestion des chaines

--- src/freetuxtv-channels-list-window.c
+++ src/freetuxtv-channels-list-window.c
@@ -12,16 +12,71 @@
 
 G_DEFINE_TYPE (FreetuxTVChannelsListWindow, freetuxtv_channels_list_window, GTK_TYPE_WINDOW);
 
+
 GtkWidget *
 freetuxtv_channels_list_window_new ()
+{
+
+	
+
+	FreetuxTVChannelsListWindow *self = NULL;
+	self = gtk_type_new (freetuxtv_channels_list_window_get_type ());
+
+	gtk_window_set_title(GTK_WINDOW(self), "Gestion des chaines");
+	gtk_window_set_default_size (GTK_WINDOW(self), 640, 480);
+	gtk_window_set_position(GTK_WINDOW(self),GTK_WIN_POS_CENTER);	
+	
+	GtkWidget *p_main_box = NULL;
+	
+	GtkWidget* label = gtk_label_new ("Gestion des chaines");
+
+	/* Creation du conteneur principal */
+	p_main_box = gtk_vbox_new (FALSE, 0);
+	gtk_container_add (GTK_CONTAINER (self), p_main_box);
+
+	/* Creation du bouton "Quitter" */
+	{
+	  GtkWidget *closeButton = NULL;
+
+	  closeButton = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
+	  g_signal_connect (G_OBJECT (closeButton), "clicked", G_CALLBACK (on_closeButton_clicked), NULL);
+	  gtk_box_pack_start (GTK_BOX (p_main_box), closeButton, FALSE, FALSE, 0);
+	}
+
+	
+
+	/* Affichage des widgets */
+	gtk_widget_show_all (GTK_WIDGET(self));
+	
+	return GTK_WIDGET(self);
+}
+
+static void
+on_closeButton_clicked(GtkWidget *widget, gpointer *data)
 {
 	FreetuxTVChannelsListWindow *self = NULL;
 	self = gtk_type_new (freetuxtv_channels_list_window_get_type ());
 
+	GtkWidget *pQuestion;
 
-	gtk_widget_show_all (GTK_WIDGET(self));
-
-	return GTK_WIDGET(self);
+	pQuestion = gtk_message_dialog_new (GTK_WINDOW(data),
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_QUESTION,
+        GTK_BUTTONS_YES_NO,
+        "Voulez vous vraiment\nquitter ce programme?");
+	/* Affichage et attente d une reponse */
+	switch(gtk_dialog_run(GTK_DIALOG(pQuestion)))
+	{
+	case GTK_RESPONSE_YES:
+            /* OUI -> on quitte l application */
+            
+            break;
+        case GTK_RESPONSE_NO:
+            /* NON -> on detruit la boite de message */
+            gtk_widget_destroy(pQuestion);
+            break;
+	}
+	
 }
 
 static void
