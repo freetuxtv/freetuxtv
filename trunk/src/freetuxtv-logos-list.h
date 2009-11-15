81626c7274aa0a65bde276a2e06a1e4dc429b310
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2009-11-15 16:32:34 +0000

    Mise à jour de la licence dans les entêtes des fichiers sources.

--- trunk/src/freetuxtv-logos-list.h
+++ trunk/src/freetuxtv-logos-list.h
@@ -1,11 +1,19 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8-*- */
 /*
- * freetuxtv
- * Copyright (C) FreetuxTV Team's 2008
- * Project homepage : http://code.google.com/p/freetuxtv/
- * 
- * freetuxtv is free software.
- * 
+ * FreetuxTV is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * FreetuxTV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Glade; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  */
 
 #ifndef FREETUXTV_LOGOS_LIST_H
