ee81b87223a09f39787b80232f73f108fb3010f4
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2010-07-03 10:44:13 +0000

    [FreetuxTV] Paramètre erreur optionnel dans libvlc-gtk.

--- trunk/lib/libvlc-gtk/gtk-libvlc-instance.c
+++ trunk/lib/libvlc-gtk/gtk-libvlc-instance.c
@@ -117,6 +117,7 @@ raise_error(GtkLibvlcInstance *self, GError** error, gpointer user_data)
 			    GTK_LIBVLC_ERROR_LIBVLC,
 			    "%s", szErrMsg);
 		}
+		libvlc_clearerr();
 	}
 #endif // LIBVLC_OLD_VLCEXCEPTION
 	
@@ -139,8 +140,11 @@ raise_error(GtkLibvlcInstance *self, GError** error, gpointer user_data)
 GtkLibvlcInstance*
 gtk_libvlc_instance_new (const gchar* vlc_args[], GError** error)
 {
-	g_return_if_fail(error != NULL);
-	g_return_if_fail(*error == NULL);
+	if(error != NULL){
+		g_return_if_fail(*error == NULL);
+	}
+
+	GError* pError = NULL;
 	
 	GtkLibvlcInstance *self = NULL;
 	self = g_object_new (GTK_TYPE_LIBVLC_INSTANCE, NULL);
@@ -176,10 +180,19 @@ gtk_libvlc_instance_new (const gchar* vlc_args[], GError** error)
 #endif // LIBVLC_OLD_INSTANCE
 
 #ifdef LIBVLC_OLD_VLCEXCEPTION
-	raise_error(self, error, &_vlcexcep);
+	raise_error(self, &pError, &_vlcexcep);
 #else
-	raise_error(self, error, NULL);	
+	raise_error(self, &pError, NULL);	
 #endif // LIBVLC_OLD_VLCEXCEPTION
+
+	if(pError){
+		if(error){
+			*error = pError;
+		}else{
+			g_error_free (pError);
+			pError = NULL;
+		}
+	}
 
 	return self;
 }
@@ -202,8 +215,9 @@ LIBVLC_INSTANCE_HANDLE
 gtk_libvlc_instance_get_libvlc_instance(GtkLibvlcInstance* pLibvlcInstance, GError** error)
 {
 	g_return_if_fail(pLibvlcInstance != NULL);
-	g_return_if_fail(error != NULL);
-	g_return_if_fail(*error == NULL);
+	if(error != NULL){
+		g_return_if_fail(*error == NULL);
+	}
 
 	LIBVLC_INSTANCE_HANDLE handle;
 	
