e9be504f254e7164d50b5c3c04ed39a1e451dae9
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2010-09-20 21:39:26 +0000

    [WebTVManager] Add yii framework as library with svn externals.

--- tools/webtv-manager/trunk/src/index-test.php
+++ tools/webtv-manager/trunk/src/index-test.php
@@ -5,7 +5,7 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../lib/yii-1.1.3.r2247/framework/yii.php';
+$yii=dirname(__FILE__).'/../../lib/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
--- tools/webtv-manager/trunk/src/index.php
+++ tools/webtv-manager/trunk/src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../lib/yii-1.1.3.r2247/framework/yii.php';
+$yii=dirname(__FILE__).'/../../lib/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
