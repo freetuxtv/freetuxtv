02ed5b437d667034cdb15ba2ffc7b92b6ab50961
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2011-09-11 21:55:32 +0000

    [FreetuxTV] Correct closing window add group with gtk3.

--- trunk/src/freetuxtv-window-add-channels-group.c
+++ trunk/src/freetuxtv-window-add-channels-group.c
@@ -794,5 +794,10 @@ do_close_clicked (gpointer user_data)
 static void
 on_buttonclose_clicked (GtkButton *button, gpointer user_data)
 {
+#if GTK_API_VERSION == 3
+	// TODO : Find how to send it in idle
+	do_close_clicked(user_data);
+#else
 	g_idle_add (do_close_clicked, user_data);
+#endif
 }
