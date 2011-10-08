14fe04823ae59bf27f05da9d0ce87dfa6dc067db
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2011-10-08 17:56:11 +0000

    [WebTVManager] Comment on the importer.

--- tools/webtv-manager/trunk/src/protected/views/webStreamImport/index.php
+++ tools/webtv-manager/trunk/src/protected/views/webStreamImport/index.php
@@ -24,3 +24,28 @@
 
 <?php echo $this->renderPartial('_importform', array('modelImportForm'=>$modelImportForm)); ?>
 
+<br/>
+
+<p>
+The format WSLFE for "Web Stream Link File Exchange" used by the importer, in an XML file that look the following example :
+<br/>
+<?php
+	$xmlDesc = '
+<?xml version="1.0" encoding="UTF-8"?>
+<webstream_link_exchange xmlns:wslfe="wslfe">
+	<wslfe:sequence source="datasource_name">
+		<wslfe:stream name="Channel 1">mms://mywebsite.com/channel1.wsx</wslfe:stream>
+		<wslfe:stream name="Channel 2">rtsp://channel2.com/live</wslfe:stream>
+		<wslfe:stream name="Channel International">http://192.168.0.1/stream</wslfe:stream>
+	</wslfe:sequence>
+</webstream_link_exchange>';
+
+echo str_replace("\t", "&nbsp;&nbsp;", str_replace("\n", "<br/>", htmlentities($xmlDesc)));
+?>
+
+<br/>
+<br/>
+If you have a link database and compare it to our database you can just create a WSLFE file, put it on a Web Server and pass the link to the import module.
+You will get the list of links and see if the links are already present in the database. If not, you can send us your WSLFE file (or URL), and we can import all the link in the database. 
+</p>
+
