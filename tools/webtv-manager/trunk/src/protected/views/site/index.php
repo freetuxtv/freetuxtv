70c5dba0fd4dc0f3c447c8a1976a23a51ab9003d
eric.beuque <eric.beuque@fbc84fb2-5c42-0410-b188-5b7b47c1c183>
2011-10-10 19:43:22 +0000

    [WebTVManager] Change index page.

--- tools/webtv-manager/trunk/src/protected/views/site/index.php
+++ tools/webtv-manager/trunk/src/protected/views/site/index.php
@@ -6,7 +6,9 @@
 
 <p>You can look for a channel by using the search form :</p>
 
-<div class="wide form">
+<div class="table">
+
+	<div class="tbody">
 
 <?php
 	$form=$this->beginWidget('CActiveForm', array(
@@ -17,62 +19,71 @@
 		));
 ?>
 
-	<div class="row">
-		<?php
-			echo $form->labelEx($modelSearchForm,'Name');
-			echo $form->textField($modelSearchForm,'Name',array('size'=>20,'maxlength'=>20));
-			echo $form->error($modelSearchForm,'Name');
-		?>
-	</div>
+		<div class="tr">
+			<div class="td">
+				<?php
+					echo $form->labelEx($modelSearchForm,'Name');
+					echo $form->textField($modelSearchForm,'Name',array('size'=>20,'maxlength'=>20));
+					echo $form->error($modelSearchForm,'Name');
+				?>
+			</div>
 
-	<div class="row">
-		<?php
-			echo $form->labelEx($modelSearchForm, 'Type');
-			$dropList = WebStream::getTypeStreamList();
-			echo $form->dropDownList($modelSearchForm, 'Type', $dropList, array ('empty' => '-- All --'));
-			echo $form->error($modelSearchForm, 'Type');
-		?>
-	</div>
+			<div class="td">
+				<?php
+					echo $form->labelEx($modelSearchForm, 'Type');
+					$dropList = WebStream::getTypeStreamList();
+					echo $form->dropDownList($modelSearchForm, 'Type', $dropList, array ('empty' => '-- All --'));
+					echo $form->error($modelSearchForm, 'Type');
+				?>
+			</div>
 
-	<div class="row">
-		<?php
-			echo $form->labelEx($modelSearchForm, 'Language');
-			$dropList = CHtml::listData(Lang::model()->findAll(array('order'=>'Label')), 'Code', 'Label');
-			$dropList = array_merge(array('none' => '- None -'), $dropList);
-			echo $form->dropDownList($modelSearchForm, 'Language', $dropList, array ('empty' => '-- All --'));
-			echo $form->error($modelSearchForm, 'Language');
-		?>
-	</div>
-	
-	<div class="row">
-		<?php
-			echo $form->labelEx($modelSearchForm, 'Country');
-			$dropList = CHtml::listData(Country::model()->findAll(array('order'=>'Label')), 'Code', 'Label');
-			$dropList = array_merge(array('none' => '- None -'), $dropList);
-			echo $form->dropDownList($modelSearchForm, 'Country', $dropList, array ('empty' => '-- All --'));
-			echo $form->error($modelSearchForm, 'Country');
-		?>
-	</div>
+			<div class="td">
+				<?php
+					echo $form->labelEx($modelSearchForm, 'Language');
+					$dropList = CHtml::listData(Lang::model()->findAll(array('order'=>'Label')), 'Code', 'Label');
+					$dropList = array_merge(array('none' => '- None -'), $dropList);
+					echo $form->dropDownList($modelSearchForm, 'Language', $dropList, array ('empty' => '-- All --'));
+					echo $form->error($modelSearchForm, 'Language');
+				?>
+			</div>
+		</div>
+		<div class="tr">
+					
+			<div class="td">
+				<?php
+					echo $form->labelEx($modelSearchForm, 'Country');
+					$dropList = CHtml::listData(Country::model()->findAll(array('order'=>'Label')), 'Code', 'Label');
+					$dropList = array_merge(array('none' => '- None -'), $dropList);
+					echo $form->dropDownList($modelSearchForm, 'Country', $dropList, array ('empty' => '-- All --'));
+					echo $form->error($modelSearchForm, 'Country');
+				?>
+			</div>
 
-	<div class="row">
-		<?php
-			echo $form->labelEx($modelSearchForm, 'Status');
-			$dropList = CHtml::listData(StreamStatus::model()->findAll('Searchable=TRUE'), 'Code', 'Label');
-			echo $form->dropDownList($modelSearchForm, 'Status', $dropList, array (
-				'empty' => '-- All --',
-				'options' => array(WebStream::WEBSTREAM_STATUS_WORKING => array('selected' => true))
-			));
-			echo $form->error($modelSearchForm, 'Status');
-		?>
-	</div>
+			<div class="td">
+				<?php
+					echo $form->labelEx($modelSearchForm, 'Status');
+					$dropList = CHtml::listData(StreamStatus::model()->findAll('Searchable=TRUE'), 'Code', 'Label');
+					echo $form->dropDownList($modelSearchForm, 'Status', $dropList, array (
+						'empty' => '-- All --',
+						'options' => array(WebStream::WEBSTREAM_STATUS_WORKING => array('selected' => true))
+					));
+					echo $form->error($modelSearchForm, 'Status');
+				?>
+			</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
+			<div class="td">
+				<br/>
+				<?php echo CHtml::submitButton('Search'); ?>
+			</div>
+		</div>
+		
 <?php $this->endWidget(); ?>
 
+	</div>
+	
 </div><!-- search-form -->
+
+<br/>
 
 <?php
 	if(Yii::app()->user->checkAccess('sendWebStream')) {
