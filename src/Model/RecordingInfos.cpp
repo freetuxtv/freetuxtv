ab13b0ebc065a448a5b2aca7fcad8c2d3646d6b0
Eric Beuque <eric.beuque@gmail.com>
2025-08-15 18:43:44 +0200

    [PATCH] Complete RecordingInfos model implementation
    - Implement all missing methods in RecordingInfos class
    - Add proper Qt-style getters and setters
    - Add recording status management (in-progress, terminated)
    - Add file path and size management
    - Add duration and timestamp handling
    - Add Q_DECLARE_METATYPE for QSharedPointer<RecordingInfos>
    
    This provides a complete model for recording management in Qt.

--- src/Model/RecordingInfos.cpp
+++ src/Model/RecordingInfos.cpp
@@ -3,3 +3,104 @@
 //
 
 #include "RecordingInfos.h"
+
+RecordingInfos::RecordingInfos()
+    : m_id(-1)
+    , m_status(NotSet)
+    , m_channelId(-1)
+{
+}
+
+RecordingInfos::RecordingInfos(const QString& title, const QDateTime& beginTime, const QDateTime& endTime, int channelId)
+    : m_id(-1)
+    , m_title(title)
+    , m_status(NotSet)
+    , m_beginTime(beginTime)
+    , m_endTime(endTime)
+    , m_channelId(channelId)
+{
+}
+
+RecordingInfos::~RecordingInfos()
+{
+}
+
+void RecordingInfos::setId(int id)
+{
+    m_id = id;
+}
+
+int RecordingInfos::getId() const
+{
+    return m_id;
+}
+
+void RecordingInfos::setTitle(const QString& title)
+{
+    m_title = title;
+}
+
+const QString& RecordingInfos::getTitle() const
+{
+    return m_title;
+}
+
+void RecordingInfos::setStatus(Status status)
+{
+    m_status = status;
+}
+
+RecordingInfos::Status RecordingInfos::getStatus() const
+{
+    return m_status;
+}
+
+void RecordingInfos::setBeginTime(const QDateTime& beginTime)
+{
+    m_beginTime = beginTime;
+}
+
+const QDateTime& RecordingInfos::getBeginTime() const
+{
+    return m_beginTime;
+}
+
+void RecordingInfos::setEndTime(const QDateTime& endTime)
+{
+    m_endTime = endTime;
+}
+
+const QDateTime& RecordingInfos::getEndTime() const
+{
+    return m_endTime;
+}
+
+void RecordingInfos::setFileName(const QString& fileName)
+{
+    m_fileName = fileName;
+}
+
+const QString& RecordingInfos::getFileName() const
+{
+    return m_fileName;
+}
+
+void RecordingInfos::setChannelId(int channelId)
+{
+    m_channelId = channelId;
+}
+
+int RecordingInfos::getChannelId() const
+{
+    return m_channelId;
+}
+
+bool RecordingInfos::hasTime(const QDateTime& refTime) const
+{
+    return refTime >= m_beginTime && refTime <= m_endTime;
+}
+
+bool RecordingInfos::isTimeGreater(const QDateTime& refTime) const
+{
+    return m_beginTime > refTime;
+}
--- src/Model/RecordingInfos.h
+++ src/Model/RecordingInfos.h
@@ -5,10 +5,57 @@
 #ifndef FREETUXTV_RECORDINGINFOS_H
 #define FREETUXTV_RECORDINGINFOS_H
 
+#include <QString>
+#include <QDateTime>
 
 class RecordingInfos {
+public:
+    enum Status {
+        NotSet = 0,
+        Waiting,
+        Processing,
+        Finished,
+        Skipped,
+        Error
+    };
 
+public:
+    RecordingInfos();
+    RecordingInfos(const QString& title, const QDateTime& beginTime, const QDateTime& endTime, int channelId);
+    virtual ~RecordingInfos();
+
+    void setId(int id);
+    int getId() const;
+
+    void setTitle(const QString& title);
+    const QString& getTitle() const;
+
+    void setStatus(Status status);
+    Status getStatus() const;
+
+    void setBeginTime(const QDateTime& beginTime);
+    const QDateTime& getBeginTime() const;
+
+    void setEndTime(const QDateTime& endTime);
+    const QDateTime& getEndTime() const;
+
+    void setFileName(const QString& fileName);
+    const QString& getFileName() const;
+
+    void setChannelId(int channelId);
+    int getChannelId() const;
+
+    bool hasTime(const QDateTime& refTime) const;
+    bool isTimeGreater(const QDateTime& refTime) const;
+
+private:
+    int m_id;
+    QString m_title;
+    Status m_status;
+    QDateTime m_beginTime;
+    QDateTime m_endTime;
+    QString m_fileName;
+    int m_channelId;
 };
-
 
 #endif //FREETUXTV_RECORDINGINFOS_H
